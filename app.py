c51b3247657af233e47648a27d808cee6843557a
indraPLM <112387467+indraPLM@users.noreply.github.com>
2026-03-03 10:50:12 +0700

    Update app.py

--- app.py
+++ app.py
@@ -40,16 +40,17 @@ if api_key:
         "📈 Tide Tsunami Cat"
     ])
 
-    #with tab1:
-    #    regional_map.show(api_key)
+    with tab1:
+        regional_map.show(api_key)
 
     #with tab2:
     #    earthquake_events.show(api_key)
 
-    with tab3:
-        tsunami_wave_cat.show(api_key)
+    #with tab3:
+    #    tsunami_wave_cat.show(api_key)
 else:
     st.warning("Please enter your IOC API Key to continue.")
+
 
 
 
