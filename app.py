9f958f0869db78b3cd92ea5f6d31a0481513be44
indraPLM <112387467+indraPLM@users.noreply.github.com>
2026-03-03 10:52:16 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import regional_map
 import earthquake_events
-#import tsunami_wave_cat
+import tsunami_wave_cat
 
 # Wide layout ensures tabs stretch across the page
 st.set_page_config(page_title="Tsunami & Tide Gauge Dashboard", layout="wide")
@@ -40,16 +40,17 @@ if api_key:
         "📈 Tide Tsunami Cat"
     ])
 
-    with tab1:
-        regional_map.show(api_key)
+    #with tab1:
+    #    regional_map.show(api_key)
 
     #with tab2:
     #    earthquake_events.show(api_key)
 
-    #with tab3:
-    #    tsunami_wave_cat.show(api_key)
+    with tab3:
+        tsunami_wave_cat.show(api_key)
 else:
     st.warning("Please enter your IOC API Key to continue.")
+
 
 
 
