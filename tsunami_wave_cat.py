46e6e0aa476e65a4d3404b534411fc28597fe971
indraPLM <112387467+indraPLM@users.noreply.github.com>
2026-03-03 10:54:19 +0700

    Update tsunami_wave_cat.py

--- tsunami_wave_cat.py
+++ tsunami_wave_cat.py
@@ -250,7 +250,11 @@ def show(api_key):
         stations_df = get_stations(api_key)
 
         closest = get_closest_stations(stations_df, tsu_lat, tsu_lon)
-        st.dataframe(closest[["Code", "Location", "country", "distance_km"]].reset_index(drop=True).rename_axis("Index").set_index(pd.RangeIndex(1, len(closest)+1))
+        df_display = closest[["Code", "Location", "country", "distance_km"]].copy()
+        df_display.index = range(1, len(df_display) + 1)
+        st.dataframe(df_display)
+
+        #st.dataframe(closest[["Code", "Location", "country", "distance_km"]].reset_index(drop=True).rename_axis("Index").set_index(pd.RangeIndex(1, len(closest)+1))
         # Reset index to start at 1 before displaying
 
         st.plotly_chart(build_closest_graphs(api_key, closest, event_time), use_container_width=True)
